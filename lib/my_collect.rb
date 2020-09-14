542aae438b753ea6155ec1f1ee489e214539eb72
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 01:02:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,3 @@
-# def my_collect(array)
-#   names = []
-#   languages = []
-  
-
-# end
 def my_collect(array)
-array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect(array) do |name|
-  name.split(" ").first
-end
+
 end
