149c5ac806c85e782ae4bfdc5308212078ef508c
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 01:10:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+list = ["Bingo Bango", "Taylor Booth", "Judy Booty"]
 
 
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
